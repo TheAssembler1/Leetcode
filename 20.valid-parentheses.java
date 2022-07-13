5724f970212f79e92610669355cfcc5064740555
TheAssembler1 <lewisnoah592@gmail.com>
2022-07-12 19:15:28 -0500

    gottem

--- 20.valid-parentheses.java
+++ 20.valid-parentheses.java
@@ -8,6 +8,37 @@
 import java.util.Stack;  
 class Solution {
     public boolean isValid(String s) {
+        String[] validStrings = {"()", "{}", "[]"};
+
+
+        Stack<Character> stack = new Stack<Character>();
+
+        for(int i = 0; i < s.length(); i++){
+            if(stack.isEmpty()){
+                char currentChar = s.charAt(i);
+
+                if(currentChar != '(' && currentChar != '{' && currentChar != '[')
+                    return false;
+
+                stack.push(currentChar);
+            }else{
+                char currentChar = s.charAt(i);
+
+                if(currentChar == '(' || currentChar == '{' || currentChar == '[')
+                    stack.push(currentChar);
+                else{
+                    String currentString = Character.toString(stack.pop()) + Character.toString(s.charAt(i));
+
+                    if(currentString.compareTo("()") != 0 && currentString.compareTo("{}") != 0 && currentString.compareTo("[]") != 0)
+                        return false;
+                }
+            }
+        }
+
+        if(!stack.isEmpty())
+            return false;
+
+        return true;
     }
 }
 // @lc code=end
